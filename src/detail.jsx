321e99773cefb329ccbb140a31879cfccf90c81e
JinChaeKim <rlawlsco88@gmail.com>
2025-02-11 22:59:39 +0900

    김진채 내용 추가

--- src/detail.jsx
+++ src/detail.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const detail = () => {
-  return <div>This is detail page</div>;
+  return (
+    <>
+      <div>김진채-login branch에서 내용 추가</div>
+      <div>This is detail page</div>
+    </>
+  );
 };
 
 export default detail;
